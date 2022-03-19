45f67330c409e43114c025ab19ee9bb437808513
unknown <kevin.paredescaman20@gmail.com>
2022-03-18 20:18:19 -0500

    sF

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,16 +1,20 @@
-import bcrypt from 'bcryptjs'
+import bcrypt from "bcryptjs";
 export interface IUser {
-    username: string
-    email: string
-    password: string
-    //validatePassword(password: string): Promise<boolean>
-};
+  username: string;
+  email: string;
+  password: string;
+  //validatePassword(password: string): Promise<boolean>
+  //solving fryday
+}
 
 export const encrypPassword = async (password: string): Promise<string> => {
-    const salt = await bcrypt.genSalt(10);
-    return bcrypt.hash(password, salt);
+  const salt = await bcrypt.genSalt(10);
+  return bcrypt.hash(password, salt);
 };
 
-export const validatePassword = async function (password: string, contrasenia: string): Promise<boolean> {
-    return await bcrypt.compare(password, contrasenia);
+export const validatePassword = async function (
+  password: string,
+  contrasenia: string
+): Promise<boolean> {
+  return await bcrypt.compare(password, contrasenia);
 };
